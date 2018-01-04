0b7d1d773556894f1517c008979975f694a9e2d6
Ralph Williams <rwilliams.b3@gmail.com>
2018-01-04 13:35:02 -0800

    deleted the grabber function

--- githuh_api.js
+++ githuh_api.js
@@ -3,43 +3,21 @@ var profile = require("./profile");
 var repos = require("./repos");
 var stars = require("./stars");
 
-var username;
-var command; //command
 
 
-function grabber(x){
-  var index = process.argv.indexOf(x);
-  //if "profile" is in the process store it's position as index. 
-  if(index === -1){
-     return null;
-  } else {
-    command = process.argv[index];
-     return process.argv[index + 1];
-  }
-} //it updates the variable username
-//takes a username input updates username
-
-
-
-
-var profileTrue = grabber("profile"); //true
-var reposTrue = grabber("repos");
-var starsTrue = grabber("stars");
-
+var command = process.argv[2];
+var username = process.argv[3];
 
 
     if(command === "profile"){
-      username = profileTrue;
         console.log(`Thank you for using the profile command.`);
         console.log(`Gathering Profile Details for: ${username}`);
         profile(username);
     } else if(command === "repos"){
-         username = reposTrue;
         console.log(`Thank you for using the repos command.`);
         console.log(`Gathering repos for: ${username}`);
         repos(username);
     } else if(command === "stars"){
-        username = starsTrue;
         console.log(`Thank you for using the stars command.`);
         console.log(`Gathering repos for: ${username}`);
         stars(username);
